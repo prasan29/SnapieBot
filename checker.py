e33f4a687ba93e90c023afff245f34d81a1c7a29
Prasanna Srinivasan <prasansrini29@gmail.com>
2020-06-28 12:38:26 +0530

    Test commit for .gitignore.

---
+++ checker.py
@@ -0,0 +1,4 @@
+import telegram
+bot = telegram.Bot(token="1163069375:AAFHFK6RlJ2w10dn89xoMfhJxjBm4c_oxD8")
+
+print(bot.getMe())
--- telebot/credentials.py
+++ telebot/credentials.py
@@ -1,3 +1,6 @@
 bot_token = ""
 bot_user_name = "SnapieBot"
 URL = "http://127.0.0.1:5000/"
+
+NGROK_URL = "https://59f4ff8599c7.ngrok.io"
+BASE_TELEGRAM_URL = "https://api.telegram.org/bot{}".format(bot_token)
